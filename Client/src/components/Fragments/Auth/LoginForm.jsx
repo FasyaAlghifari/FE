dc57693e4121c4092736797362db64de8d0fe9f1
Fasya <fasyaalghifari006@gmail.com>
2024-11-12 17:11:50 +0700

    fix

--- Client/src/components/Fragments/Auth/LoginForm.jsx
+++ Client/src/components/Fragments/Auth/LoginForm.jsx
@@ -12,7 +12,7 @@ export function LoginForm() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8080/login", {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
         email,
         password,
       });
